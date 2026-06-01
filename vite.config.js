4ff7b75ce9d54450c5d2260cf42dd8df60805f48
denis-gavrilenko0910 <denys.gavrylenko@gmail.com>
2026-06-01 15:22:30 +0300

    add: npm update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/goit-react-hw-01-components/',
   plugins: [
     react({
       babel: {
